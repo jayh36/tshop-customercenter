8d37ab0f53016020676120ba5c712c61f4391d68
jayh36 <jhong0306@naver.com>
2020-09-14 17:39:45 +0900

    first commit

---
+++ src/main/java/Tshop/ReservationPage.java
@@ -0,0 +1,57 @@
+package Tshop;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name="ReservationPage")
+public class ReservationPage {
+
+        @Id
+        @GeneratedValue(strategy=GenerationType.AUTO)
+        private Long id;
+        private Long reservationId;
+        private Long productId;
+        private String status;
+        private String agencyName;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getReservationId() {
+        return reservationId;
+    }
+
+    public void setReservationId(Long reservationId) {
+        this.reservationId = reservationId;
+    }
+
+    public Long getProductId() {
+        return productId;
+    }
+
+    public void setProductId(Long productId) {
+        this.productId = productId;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getAgencyName() {
+        return agencyName;
+    }
+
+    public void setAgencyName(String agencyName) {
+        this.agencyName = agencyName;
+    }
+}
